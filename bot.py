e6ad45567734bbcaba7fdfa0e6d609cf698033a1
Yaseinansaree <106384005+Yaseinansaree@users.noreply.github.com>
2022-09-11 12:45:23 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from rubpy import Bot
 from requests import get
 from _thread import start_new_thread
+import time
 
 def answer(bot,chat,msg,):
 	try:
@@ -12,7 +13,9 @@ answered=[]
 bot_guid=("u0DQXtB0dec2771cdd3897c499a8f24b")
 while(1):
 	try:
+		time(7)
 		for chat in bot.getChatsUpdate():
+			
 				if 'SendMessages' in chat['access'] and chat['last_message']['type']=='Text':
 					msg:str=chat['last_message']['text']
 					if not chat['object_guid']+chat['last_message']['message_id']in answered:
