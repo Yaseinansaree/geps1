27fe268e4c98395d9474209a28f51c0e61e3c00e
Yaseinansaree <106384005+Yaseinansaree@users.noreply.github.com>
2022-10-06 18:17:06 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,14 +8,21 @@ def answer(bot,chat,msg,):
 		bot.sendMessage(chat['object_guid'], get('http://api.hajiapi.tk/sokhan?text='+msg).text, message_id=chat['last_message']['message_id'])
 	except:pass
 	
-bot=Bot(auth="ufmzlqqnvusrycuxbdejfgxwlrgwbond",)
+bot=Bot(auth="euinmtdmqrcissljzgnrfvzsibmzjoox",)
 answered=[]
-bot_guid=("u0DQXtB0dec2771cdd3897c499a8f24b")
+bot_guid=("u0Bv9Lp07dd06c9dc066f08ab2f2ed07")
+def block(bit,chat,text):
+	while True:
+		time.sleep(.1)
+		try:
+			bot.block(chat['object_guid'])
+			break
+		except:
+			continue
 while(1):
 	try:
-		time(7)
 		for chat in bot.getChatsUpdate():
-			
+			if chat['abs_object']['type']=='User'or chat['abs_object']['type']=='Group':
 				if 'SendMessages' in chat['access'] and chat['last_message']['type']=='Text':
 					msg:str=chat['last_message']['text']
 					if not chat['object_guid']+chat['last_message']['message_id']in answered:
@@ -23,4 +30,9 @@ while(1):
 							if msg:
 								start_new_thread(answer,(bot,chat,msg,))
 							answered.append(chat['object_guid']+chat['last_message']['message_id'])
+							text = text.strip()
+			if text == ("k"):
+				Thread(target=block,args=(bit, chat, text)).start()
+				list_message_seened.append(m_id)
+				break
 	except:pass
